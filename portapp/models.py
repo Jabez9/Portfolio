0506ef41583d297329729bfd88e183fc3e5641e2
Jabez9 <mukoshijabez@gmail.com>
2024-11-12 18:03:25 +0300

    some changes to affe

--- portapp/models.py
+++ portapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Portapp(models.Model):
+
+    name= models.CharField(max_length=100, blank = False)
+
+    email = models.CharField(max_length = 100)
+
+    service = models.CharField(max_length= 50)
+
+    message = models.TextField()
