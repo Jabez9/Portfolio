028681ec32888fea1b31e1d115fb8ebc1a9becbd
Jabez9 <mukoshijabez@gmail.com>
2024-11-21 14:45:16 +0300

    settings update

--- Huya/settings.py
+++ Huya/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j4zsh%$8-ece3cz4=y#!_0ol)_ziv7m*ix7#fi2#7%2*u2%@)n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    '9b7b-196-207-134-211.ngrok-free.app'
+    '9b7b-196-207-134-211.ngrok-free.app',
+    'portfolio-tg0p.onrender.com'
 
 
 ]
