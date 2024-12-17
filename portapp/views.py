e8aee13be3f599aefc01eae5100bd68669709c02
Jabez9 <mukoshijabez@gmail.com>
2024-12-17 17:21:36 +0300

    modidied the greeting

--- portapp/views.py
+++ portapp/views.py
@@ -1,5 +1,20 @@
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    # Get the current hour
+    current_hour = datetime.now().hour
+
+    # Determine the greeting based on the time
+    if current_hour < 12:
+        greeting = "Good Morning!"
+    elif 12 <= current_hour < 18:
+        greeting = "Good Afternoon!"
+    else:
+        greeting = "Good Evening!"
+
+    # Pass the greeting to the template
+    context = {
+        'greeting': greeting
+    }
+    return render(request, 'index.html', context)
